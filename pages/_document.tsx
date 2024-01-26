9e6e3d3f63d326d8ae88db1bf4e6c1b850dbeee5
Evgeniy Smirnov <59017716+mrlimax1@users.noreply.github.com>
2024-01-27 01:48:20 +0500

    seo fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
       <Head />
       <body>
         <Main />
