146eff9a9f26813fc2e7fca58acdc0d7869c2497
Evgeniy Smirnov <59017716+mrlimax1@users.noreply.github.com>
2024-01-26 17:51:45 +0500

    fix

--- components/ui/ExcellenceCard/index.tsx
+++ components/ui/ExcellenceCard/index.tsx
@@ -13,7 +13,7 @@ export default function ExcellenceCard({tagText, title, content} : IExcellenceCa
         >{content.text}</h1>
 
     return (
-        <div className={s.ExcellenceCard} data-aos="fade-up" data-aos-anchor-placement="center-center">
+        <div className={s.ExcellenceCard} data-aos="fade-up" data-aos-anchor-placement="bottom-center">
             <p>{ tagText }</p>
             <div>
                 { Content }
