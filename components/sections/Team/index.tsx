d1854a54a7bd4dc165fffc8749c36ff78b1316fb
Evgeniy Smirnov <59017716+mrlimax1@users.noreply.github.com>
2024-01-26 17:52:25 +0500

    fix

--- components/sections/Team/index.tsx
+++ components/sections/Team/index.tsx
@@ -33,7 +33,7 @@ export default function Team() {
                 className={s.SearchMembers}
                 href={"/"}
                 data-aos="fade-up"
-                data-aos-anchor-placement="top-center"
+                data-aos-anchor-placement="bottom-center"
             >
                 <h3>
                 Набираем в команду
