4bc1247548f8e79a60bd8b19b01b4730953826a0
Evgeniy Smirnov <59017716+mrlimax1@users.noreply.github.com>
2024-01-27 01:11:47 +0500

    metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Document() {
   return (
@@ -7,6 +8,27 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(96257197, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+              `,
+            }}
+          />
+          <GoogleTagManager gtmId="GTM-KMWB5SD5"/>
+          <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/96257197" style={{ position:'absolute', left:'-9999px' }} alt="" />
+          </div>
+        </noscript>
       </body>
     </Html>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,20 +10,50 @@ import Team from '@/components/sections/Team';
 import BurgerModal from '@/components/BurgerModal';
 import { useState } from 'react';
 
+import Head from 'next/head';
+
 export default function Home() {
   const [activeMenu, setActiveMenu] = useState(false)
 
   return (
-    <main
-      className={`${mont.className}`}
-    >
-      <BurgerModal active={activeMenu} setActive={setActiveMenu}/>
-      <Header activeMenu={activeMenu} setActiveMenu={setActiveMenu}/>
-      <Main />
-      <Case />
-      <Excellence />
-      <Team />
-      <Footer/>
-    </main>
+    <>
+      <Head>
+        <title>Вебериум | Студия вёрстки</title>
+        <meta 
+          name="description" 
+          content="Студия вёрстки ваших проектов. Разрабатываем сайты, работаем с дизайнерами и совместно приходим к идеалу." 
+        />
+        <meta name="theme-color" content="#000"/>
+        <meta name="keywords" content="студия, вёрстка, студия вёрстки, дизайн, студия дизайна, сверстать сайт, заказать сайт, сайт, купить сайт, разработка сайта, сколько стоит сайт" />
+        <meta property="og:type" content="article" />
+        <meta name="twitter:card" content="@summary_large_image" />
+        <meta name="twitter:title" content="Вебериум | Студия вёрстки" />
+        <meta name="twitter:image" content="https://вебериум.рф/ogimage.png" />
+        <meta property="og:title" content="Вебериум | Студия вёрстки" />
+        <meta property="og:image:width" content="968" />
+        <meta property="og:image:height" content="544" />
+        <meta property="og:description" content="Студия вёрстки ваших проектов. Разрабатываем сайты, работаем с дизайнерами и совместно приходим к идеалу." />
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,viewport-fit=cover"></meta>
+        <meta property="og:image" content="https://вебериум.рф/ogimage.png" />
+        <meta property="og:url" content="https://вебериум.рф"></meta>
+        <link rel="canonical" href="https://вебериум.рф" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+      </Head>
+      <main
+        className={`${mont.className}`}
+      >
+        <BurgerModal active={activeMenu} setActive={setActiveMenu}/>
+        <Header activeMenu={activeMenu} setActiveMenu={setActiveMenu}/>
+        <Main />
+        <Case />
+        <Excellence />
+        <Team />
+        <Footer/>
+      </main>
+    </>
   );
 }
