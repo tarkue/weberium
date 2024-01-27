f136749d0fe6f93e090c440fdcd7d11b9c4459ee
Evgeniy Smirnov <59017716+mrlimax1@users.noreply.github.com>
2024-01-27 21:06:50 +0500

    icon meta add

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,6 +42,7 @@ export default function Home() {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
       </Head>
       <main
